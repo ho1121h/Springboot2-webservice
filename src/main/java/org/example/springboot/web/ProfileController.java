6689bed80e2d7f4d55db080df8f0aa7e0eafb815
ho1121h <ho316s@naver.com>
2022-12-26 21:48:26 +0900

    Update ProfileController.java

--- src/main/java/org/example/springboot/web/ProfileController.java
+++ src/main/java/org/example/springboot/web/ProfileController.java
@@ -1,13 +1,13 @@
 package org.example.springboot.web;
 
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
+
 
 @RequiredArgsConstructor
 @RestController
@@ -16,12 +16,10 @@ public class ProfileController {
 
     @GetMapping("/profile")
     public String profile() {
-        List<String> profiles = Arrays.asList(env.
-                                getActiveProfiles()); // 현재실행중인 Active를 모두 가져옴
-        List<String> realProfiles = Arrays.asList("real",
-                                    "real1", "real2");
-        String defaultProfile = profiles.isEmpty() ? "default"
-                                                : profiles.get(0);
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+
         return profiles.stream()
                 .filter(realProfiles::contains)
                 .findAny()
