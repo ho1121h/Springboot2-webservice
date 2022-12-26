99f6aacd9c3e5d35f6b32a680c6011c7da07d5cc
ho1121h <ho316s@naver.com>
2022-12-26 21:42:59 +0900

    Update ProfileControllerTest.java

--- src/test/java/org/example/springboot/web/ProfileControllerTest.java
+++ src/test/java/org/example/springboot/web/ProfileControllerTest.java
@@ -24,7 +24,7 @@ public class ProfileControllerTest {
 
     @Test
     public void profile은_인증없이_호출된다() throws Exception {
-        String expected = "oauth";
+        String expected = "default";
 
         ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
