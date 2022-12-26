0d115c36d846b0fe4368a6376312280ce0a063df
ho1121h <ho316s@naver.com>
2022-12-26 21:38:17 +0900

    5

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,13 +20,17 @@ else
 fi
 
 echo "> 새 애플리케이션 배포"
+
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR_NAME: $JAR_NAME"
+
 echo "> $JAR_NAME 에 실행권한 추가"
+
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
+
 nohup java -jar \
   -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
   -Dspring.profile.active=real \
